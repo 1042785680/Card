fea2ed1d3b2b3b4de2f733ad6df805f71d83ad2a
1042785680 <1042785680@qq.com>
2018-10-06 18:18:00 +0800

    Initial commit

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+
+    public GameObject ForceBall;
+
+    public Vector3 ForcePosition;
+
+    void Start () {
+
+        Invoke("Force", 1);
+	}
+	
+	void Update () {
+
+        //if (Input.GetKeyDown(KeyCode.Z))
+        //{
+        //    Force();
+        //}
+	}
+
+    void Force()
+    {
+        //gameObject.GetComponent<Rigidbody>().AddForceAtPosition(new Vector3(ForceBall.transform.position.x - transform.position.x,
+        //    100, ForceBall.transform.position.z - transform.position.z), new Vector3(0, 0, 0));
+
+        gameObject.GetComponent<Rigidbody>().AddForceAtPosition(new Vector3((transform.position.x - ForceBall.transform.position.x) * 30,
+            200, (transform.position.z - ForceBall.transform.position.z) * 10), ForceBall.transform.position);
+    }
+
+}
